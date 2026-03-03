f60b4df98dd1aef079e9d88bf74936a1a32cea34
cad6325 <cad6325@psu.edu>
2026-03-03 18:17:01 -0500

    Committed more of GUI main page and the getters

--- src/IST261Project/Frontend/AppView.java
+++ src/IST261Project/Frontend/AppView.java
@@ -9,9 +9,17 @@ public class AppView {
     private JButton buttonRemove;
     private JButton buttonAdd;
     private JButton buttonPurchase;
-    private JButton buttonSearch;
     private JButton buttonFilter;
     private JButton buttonResetView;
+    private JLabel labelPrice;
+    private JLabel labelYear;
+    private JLabel labelEngineSize;
+    private JLabel labelMake;
+    private JLabel labelMiles;
+    private JLabel labelBodyStyle;
+    private JLabel labelModel;
+    private JLabel labelGasMileage;
+    private JLabel labelColor;
 
     public JPanel getMainFrame() {
         return MainFrame;
@@ -37,9 +45,6 @@ public class AppView {
         return buttonPurchase;
     }
 
-    public JButton getButtonSearch() {
-        return buttonSearch;
-    }
 
     public JButton getButtonFilter() {
         return buttonFilter;
@@ -47,5 +52,41 @@ public class AppView {
 
     public JButton getButtonResetView() {
         return buttonResetView;
+    }
+
+    public JLabel getLabelPrice() {
+        return labelPrice;
+    }
+
+    public JLabel getLabelYear() {
+        return labelYear;
+    }
+
+    public JLabel getLabelEngineSize() {
+        return labelEngineSize;
+    }
+
+    public JLabel getLabelMake() {
+        return labelMake;
+    }
+
+    public JLabel getLabelMiles() {
+        return labelMiles;
+    }
+
+    public JLabel getLabelBodyStyle() {
+        return labelBodyStyle;
+    }
+
+    public JLabel getLabelModel() {
+        return labelModel;
+    }
+
+    public JLabel getLabelGasMileage() {
+        return labelGasMileage;
+    }
+
+    public JLabel getLabelColor() {
+        return labelColor;
     }
 }
